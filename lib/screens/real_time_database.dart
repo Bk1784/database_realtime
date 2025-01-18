575982c7de25e406a5cc440f01aa80ed75d7302c
bk1784 <futonabuu456@gmail.com>
2025-01-18 11:29:34 +0700

    membuat dialog

--- lib/screens/real_time_database.dart
+++ lib/screens/real_time_database.dart
@@ -15,14 +15,61 @@ class _RealTimeDatabaseState extends State<RealTimeDatabase> {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         centerTitle: true,
-        title: Text('real time database', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+        title: Text(
+          'real time database',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
       ),
       body: Column(
-        children: [
-          
-        ],
+        children: [],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Dialog myDialog(
+      {required BuildContext context,
+      required String name,
+      required String address,
+      required VoidCallback onPressed}) {
+    return Dialog(
+      backgroundColor: Colors.blue,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.blue),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                //untuk close dialog
+                CircleAvatar(
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.close)),
+                )
+              ],
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'enter the name', hintText: 'eg bagus'
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
